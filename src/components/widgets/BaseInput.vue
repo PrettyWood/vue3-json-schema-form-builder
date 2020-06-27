6c00d409059d9ba47519475e84ae6a57008de6d8
PrettyWood <em.jolibois@gmail.com>
2020-06-28 00:35:10 +0200

    feat(widgets): add BaseInput

--- src/components/widgets/BaseInput.vue
+++ src/components/widgets/BaseInput.vue
@@ -1,21 +1,20 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 
-import { JsonSchema, UISchema } from '/@/types';
-
-import BaseInput from './BaseInput.vue';
+import { JsonSchema } from '/@/types';
 
 export default defineComponent({
-  name: 'BaseWidget',
-  components: { BaseInput },
+  name: 'BaseInput',
   props: {
     modelValue: { type: [String, Number], default: undefined },
     jsonSchema: { type: Object as PropType<JsonSchema>, required: true },
-    uiSchema: { type: Object as PropType<UISchema>, default: undefined },
   },
 });
 </script>
 
 <template>
-  <BaseInput v-bind="$props" />
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
 </template>
