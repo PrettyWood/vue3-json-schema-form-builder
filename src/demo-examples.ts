9804baf64da2f8c97c400e2359c923611fb6c0c3
PrettyWood <em.jolibois@gmail.com>
2020-07-24 10:36:18 +0200

    chore(demo): add example for numbers

--- src/demo-examples.ts
+++ src/demo-examples.ts
@@ -143,7 +143,72 @@ const EXAMPLES: Example[] = [
       "bio": "Roundhouse kicking asses since 1940",
       "password": "noneed"
     }
-  }
+  },
+  {
+    title: 'Object field - Numbers',
+    jsonSchema: {
+      type: 'object',
+      title: 'Number fields',
+      properties: {
+        number: {
+          title: 'Number',
+          type: 'number',
+        },
+        integer: {
+          title: 'Integer',
+          type: 'integer',
+        },
+        numberEnum: {
+          type: 'number',
+          title: 'Number enum',
+          enum: [1, 2, 3],
+        },
+        numberEnumRadio: {
+          type: 'number',
+          title: 'Number enum',
+          enum: [1, 2, 3],
+        },
+        integerRange: {
+          title: 'Integer range',
+          type: 'integer',
+          minimum: 42,
+          maximum: 100,
+        },
+        integerRangeSteps: {
+          title: 'Integer range (by 10)',
+          type: 'integer',
+          minimum: 50,
+          maximum: 100,
+          multipleOf: 10,
+        },
+      },
+    },
+    uiSchema: {
+      integer: {
+        'ui:widget': 'updown',
+      },
+      numberEnumRadio: {
+        // 'ui:widget': 'radio',
+        // 'ui:options': {
+        //   inline: true,
+        // },
+      },
+      integerRange: {
+        // 'ui:widget': 'range',
+      },
+      integerRangeSteps: {
+        // 'ui:widget': 'range',
+      },
+    },
+    formData: {
+      number: 3.14,
+      integer: 42,
+      numberEnum: 2,
+      numberEnumRadio: 2,
+      integerRange: 42,
+      integerRangeSteps: 80,
+    },
+  },
 ];
 
 export default EXAMPLES;
